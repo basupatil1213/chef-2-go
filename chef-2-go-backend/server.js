a0e6029815a3b58235075158f1aeb6940007aba2
Basavaraj Patil <patil.basava@outlook.com>
2023-12-13 18:22:16 -0500

    asa

--- chef-2-go-backend/server.js
+++ chef-2-go-backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import initialize from './app.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 // import paymentRoutes from './payment-route.js';
 dotenv.config();
 
@@ -12,6 +13,12 @@ const app = express();
 app.use(cors());
 
 app.use(express.json()); 
+
+app.use(express.static('./chef-2-go-frontend/build'));
+app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'chef-2-go-frontend', 'build', 'index.html'));
+}
+);
 const port = process.env.PORT;
 
 initialize(app);
